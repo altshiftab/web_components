fd50534445d223806bdd9bcb1aa17bca9deef6b6
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2025-09-23 22:57:10 +0200

    Remove webpack

--- src/switch.ts
+++ src/switch.ts
@@ -1,5 +1,5 @@
 import {css, CSSResultGroup, html, LitElement} from "lit";
-import {customElement, property, query} from "lit/decorators.js";
+import {customElement, property} from "lit/decorators.js";
 
 const switchElementName = "altshift-switch";
 
