61c8415f168957a7ec33ec7605f89c7631c42e37
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2025-10-04 20:20:09 +0200

    Fix theme toggler

--- src/theme_toggler.ts
+++ src/theme_toggler.ts
@@ -3,30 +3,18 @@ import {html, LitElement} from "lit";
 
 export const localStorageThemeKey = "theme";
 export const darkClassName = "dark";
+export const lightClassName = "light";
 export const darkThemeValue = "dark";
 export const lightThemeValue = "light";
 
-export function setThemeClassName(useDarkTheme: boolean, ...elements: HTMLElement[]) {
-    for (const element of elements) {
-        if (useDarkTheme) {
-            element.classList.add(darkClassName);
-        } else {
-            element.classList.remove(darkClassName);
-        }
-    }
-}
-
 export function prefersDarkTheme(): boolean {
-    const themeValue = localStorage.getItem(localStorageThemeKey);
-    const prefersDarkColorScheme = Boolean(window.matchMedia("(prefers-color-scheme: dark)").matches);
-
-    switch (themeValue) {
+    switch (localStorage.getItem(localStorageThemeKey)) {
         case lightThemeValue:
             return false;
         case darkThemeValue:
             return true;
         default:
-            return prefersDarkColorScheme;
+            return window.matchMedia("(prefers-color-scheme: dark)").matches;
     }
 }
 
